a265a9a073b027d763f1fd990d87f2500fd702ef
Oliver Monberg <au554208@uni.au.dk>
2018-10-18 11:09:36 +0200

    Add project files.

---
+++ SettingsLab/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SettingsLab
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            if (SettingsLab.Properties.Settings.Default.CallUpgrade)
+            {
+                SettingsLab.Properties.Settings.Default.Upgrade();
+                SettingsLab.Properties.Settings.Default.CallUpgrade = false;
+            }
+
+        }
+
+        private void Application_Exit(object sender, ExitEventArgs e)
+        {
+            SettingsLab.Properties.Settings.Default.Save();
+        }
+    }
+}
